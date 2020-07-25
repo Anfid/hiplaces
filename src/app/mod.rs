b5baf0f3c432ad7f2ecc90e1e3c538e48b2ee9a9
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2020-07-26 03:43:47 +0700

    Add API description on GET /

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -11,6 +11,110 @@ pub struct AppState {
 
 async fn api(_state: Data<AppState>, _req: HttpRequest) -> &'static str {
 r#"
+# API
+
+## Registration
+
+### Request
+
+POST /api/v1/users
+
+Content-Type: application/json
+
+```json
+{
+  "username": string,
+  "email": string,
+  "password": string
+}
+```
+
+### Response
+
+201 Created
+
+```json
+{
+  "token": string
+  "username": string,
+  "email": string
+}
+```
+
+### Errors
+
+* Invalid data -> 200 Ok kind: "field_validation"
+* User already exists -> 200 Ok kind: "already_exists"
+* Internal error -> 500 Internal Server Error
+
+
+## Login
+
+### Request
+
+POST /api/v1/users/login
+
+Content-Type: application/json
+
+```json
+{
+  "username": string,
+  "password": string
+}
+```
+
+### Response
+
+200 Ok
+
+```json
+{
+  "token": string
+  "username": string,
+  "email": string
+}
+```
+
+### Errors
+
+* Invalid data -> 200 Ok { "error": { kind: "authorization" } }
+* Internal error -> 500 Internal Server Error
+
+
+## Get current user data
+
+### Request
+
+GET /api/v1/user
+
+Authorization: <token>
+
+### Response
+
+501 Not Implemented
+
+### Errors
+
+* Bad or missing JWT token -> 401 Unauthorized
+
+
+## Update current user data
+
+### Request
+
+PUT /api/v1/user
+
+Authorization: <token>
+
+### Response
+
+501 Not Implemented
+
+### Errors
+
+* Bad or missing JWT token -> 401 Unauthorized
+
+
 # Errors
 
 Errors have the following format:
@@ -18,7 +122,7 @@ Errors have the following format:
 ```json
 {
   "error": {
-    "kind": "<error_kind>"
+    "kind": "<error_kind>",
     "info": <error_data>
   }
 }
@@ -36,7 +140,7 @@ Possible `error_kind` variants and structure of corresponding `error_data`:
 {
   "code": string,
   "message": string,
-  "params": dict,
+  "params": dict
 }
 ```
 "#
