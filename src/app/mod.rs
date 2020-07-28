184972b025a52f845a5a6e5fd31fd18062b63a47
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2020-07-28 23:22:26 +0700

    Move API description to file end

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -10,7 +10,53 @@ pub struct AppState {
 }
 
 async fn api(_state: Data<AppState>, _req: HttpRequest) -> &'static str {
-r#"
+    API_DESC
+}
+
+pub fn routes(app: &mut web::ServiceConfig) {
+    app.service(web::resource("/").route(web::get().to(api)))
+        .service(
+            web::scope("/api/v1")
+                // User routes
+                .service(web::resource("users").route(web::post().to(users::register)))
+                .service(web::resource("users/login").route(web::post().to(users::login)))
+                .service(
+                    web::resource("user")
+                        .wrap(crate::middleware::auth::Auth)
+                        .route(web::get().to(users::get_current))
+                        .route(web::put().to(users::update)),
+                ),
+                   /*
+                   .service(
+                       web::resource("locations")
+                           .route(web::get().to(locations::list))
+                           .route(web::post().to(locations::create)),
+                   )
+                   .service(
+                       web::resource("locations/{slug}")
+                           .route(web::get().to(locations::get))
+                           .route(web::put().to(locations::update))
+                           .route(web::delete().to(locations::delete)),
+                   )
+                   .service(
+                       web::resource("locations/{slug}/favorite")
+                           .route(web::post().to(locations::favorite))
+                           .route(web::delete().to(locations::unfavorite)),
+                   )
+                   .service(
+                       web::resource("locations/{slug}/comments")
+                           .route(web::get().to(locations::comments::list))
+                           .route(web::post().to(locations::comments::add)),
+                   )
+                   .service(
+                       web::resource("locations/{slug}/comments/{comment_id}")
+                           .route(web::delete().to(locations::comments::delete)),
+                   )
+                   */
+        );
+}
+
+const API_DESC: &'static str = r#"
 # API
 
 ## Registration
@@ -143,48 +189,4 @@ Possible `error_kind` variants and structure of corresponding `error_data`:
   "params": dict
 }
 ```
-"#
-}
-
-pub fn routes(app: &mut web::ServiceConfig) {
-    app.service(web::resource("/").route(web::get().to(api)))
-        .service(
-            web::scope("/api/v1")
-                // User routes
-                .service(web::resource("users").route(web::post().to(users::register)))
-                .service(web::resource("users/login").route(web::post().to(users::login)))
-                .service(
-                    web::resource("user")
-                        .wrap(crate::middleware::auth::Auth)
-                        .route(web::get().to(users::get_current))
-                        .route(web::put().to(users::update)),
-                ), // TODO: Article routes
-                   /*
-                   .service(
-                       web::resource("locations")
-                           .route(web::get().to(locations::list))
-                           .route(web::post().to(locations::create)),
-                   )
-                   .service(
-                       web::resource("locations/{slug}")
-                           .route(web::get().to(locations::get))
-                           .route(web::put().to(locations::update))
-                           .route(web::delete().to(locations::delete)),
-                   )
-                   .service(
-                       web::resource("locations/{slug}/favorite")
-                           .route(web::post().to(locations::favorite))
-                           .route(web::delete().to(locations::unfavorite)),
-                   )
-                   .service(
-                       web::resource("locations/{slug}/comments")
-                           .route(web::get().to(locations::comments::list))
-                           .route(web::post().to(locations::comments::add)),
-                   )
-                   .service(
-                       web::resource("locations/{slug}/comments/{comment_id}")
-                           .route(web::delete().to(locations::comments::delete)),
-                   )
-                   */
-        );
-}
+"#;
